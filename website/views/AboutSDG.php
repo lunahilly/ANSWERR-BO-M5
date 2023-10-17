8a12d38f85f81db575534625dda7a6dc03392ed9
lunahilly <35861@ma-web.nl>
2023-10-17 12:42:35 +0200

    Create AboutSDG.php

---
+++ website/views/AboutSDG.php
@@ -0,0 +1,13 @@
+<article class="about">
+    <h2>Wat zijn SDG's?</h2>  <!-- informatie over wat SDG's zijn + CTA button -->
+    <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+        <!-- wil je meer weten over alle SDG's? speel nu het spel! -->
+        <!-- maak kennis met alle SDG's, speel de SDG game nu! -->
+    </p>
+    <button class="cta">Play the game</button>  <!-- CTA button -->
+</article>
