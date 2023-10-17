e3a9b747b9230e0bebd43c2313a07ef3a68b83f3
lunahilly <35861@ma-web.nl>
2023-10-17 14:39:21 +0200

    ...

--- website/public/index.php
+++ website/public/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>NVVN-SDG</title>
-</head>
+</head> 
 
 <body>
     <!-- wat er in de nav staat moeten we nog bedenken -->
