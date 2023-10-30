f8a74124de4c184954fb1c840f9ff929f3708957
lunahilly <35861@ma-web.nl>
2023-10-30 14:56:18 +0100

    nav, werkende knopjes

--- website/views/AboutSDG.php
+++ website/views/AboutSDG.php
@@ -3,22 +3,18 @@
 <article class="about">
     <h2>Wat zijn SDG's?</h2>  <!-- informatie over wat SDG's zijn + CTA button -->
     <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        
-
+    De Sustainable Development Goals (SDG’s), zijn zeventien doelen om van de wereld een betere plek te maken in 2030. 
+    Ze zijn een mondiaal kompas voor uitdagingen als armoede, onderwijs en de klimaatcrisis. 
+    SDG Nederland is het netwerk van iedereen die in ons land aan de doelen wil bijdragen.
+    In 2015 hebben alle 193 landen die lid zijn van de Verenigde Naties (VN) de doelen aangenomen. 
+    Met deze doelen willen zij in 2030 een duurzame wereld voor iedereen bereiken, waarin niemand wordt buitengesloten. 
+    De doelen gelden voor alle landen en voor alle mensen.
+    <br><br>
+    Maak kennis met alle SDG's, speel de SDG game nu!
+    <br>
+    De SDG game leert je meer over alle specifieke SDG's op een leuke manier!
         <!-- wil je meer weten over alle SDG's? speel nu het spel! -->
         <!-- maak kennis met alle SDG's, speel de SDG game nu! -->
     </p>
-    <button class="cta">PLAY THE GAME!</button>  <!-- CTA button -->
+    <button class="cta"><a href="https://sdggame.nl/">PLAY THE GAME!</a></button>  <!-- CTA button -->
 </article>
--- website/views/nav.php
+++ website/views/nav.php
@@ -4,8 +4,8 @@
 <nav>
         <ul>
             <li> <img src="/img/NVVN.webp" alt="logo"> </li>
-            <li class= "menu">About SDG</li>
-            <li class="menu">PLAY THE GAME</li>
+            <li class= "menu">Over NVVN</li>  <!-- we moeten he9er nog een pagina voor maken, info: https://nvvn.nl/over-nvvn/ -->
+            <li class="menu"><a href="https://sdggame.nl/">PLAY THE GAME!</a></li>
             <!--    <i class="fa-solid fa-bars fa-2xl" style="color: #000000;"></i>  -->
         </ul>
     </nav>
