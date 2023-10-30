d5a224107f4a3c7c5df70f1cd136875de9de4fef
lunahilly <35861@ma-web.nl>
2023-10-30 14:02:37 +0100

    ...

--- website/views/footer.php
+++ website/views/footer.php
@@ -1,10 +1,26 @@
 <footer>
-    <img src="/img/NVVN2.webp" alt="logo">
+    <a href="https://nvvn.nl/">
+        <img src="/img/NVVN2.webp" alt="logo">
+    </a>
     <div class= "logos">
-        <i class="fa-brands fa-facebook" style="color: #000000;"></i>
-        <i class="fa-brands fa-twitter" style="color: #000000;"></i>
-        <i class="fa-brands fa-instagram" style="color: #000000;"></i>
-        <i class="fa-brands fa-soundcloud" style="color: #000000;"></i>
-        <i class="fa-brands fa-spotify" style="color: #000000;"></i>
+        <a href="https://www.facebook.com/UNANetherlands">
+            <i class="fa-brands fa-facebook" style="color: #000000;"></i>
+        </a>
+
+        <a href="https://twitter.com/UNANL">
+            <i class="fa-brands fa-twitter" style="color: #000000;"></i>
+        </a>
+
+        <a href="https://www.instagram.com/nederlandse_vereniging_vn/">
+            <i class="fa-brands fa-instagram" style="color: #000000;"></i>
+        </a>
+
+        <a href="https://soundcloud.com/user-522282097-468418089">
+            <i class="fa-brands fa-soundcloud" style="color: #000000;"></i>
+        </a>
+
+        <a href="https://open.spotify.com/show/5Sgn01M9cgJJygGyPT9prc">
+            <i class="fa-brands fa-spotify" style="color: #000000;"></i>
+        </a>
     </div>
 </footer>
--- website/views/nav.php
+++ website/views/nav.php
@@ -6,6 +6,6 @@
             <li> <img src="/img/NVVN.webp" alt="logo"> </li>
             <li class= "menu">About SDG</li>
             <li class="menu">PLAY THE GAME</li>
-            <i class="fa-solid fa-bars fa-2xl" style="color: #000000;"></i>
+            <!--    <i class="fa-solid fa-bars fa-2xl" style="color: #000000;"></i>  -->
         </ul>
     </nav>
