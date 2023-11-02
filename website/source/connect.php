68c3f253e75d695c1055e68b573b0ec62a55812d
lunahilly <35861@ma-web.nl>
2023-11-02 20:35:38 +0100

    Update connect.php

--- website/source/connect.php
+++ website/source/connect.php
@@ -4,14 +4,14 @@ function db_connect() {
     if (file_exists('../source/.env')) {
         $envSettings = parse_ini_file('../source/.env');
 
-        $conn = new mysqli($envSettings["DB_HOST"], $envSettings["DB_USER"], $envSettings["DB_PASSWORD"], $envSettings["DB_NAME"]);
+        $conn = mysqli_connect($envSettings["DB_NAME"], $envSettings["DB_USER"], $envSettings["DB_PASSWORD"], $envSettings["DB_HOST"]);
 
-        if ($conn -> connect_errno) {
-            echo "Failed to connect to MySQL: " . $conn -> connect_error;
-            exit();
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
         }
 
         return $conn;
+        mysqli_close();
     }
 }
 
