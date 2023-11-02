e6f1e956bb1fda020660b4b36c31173b14212278
Ryan <34758@ma-web.nl>
2023-11-02 21:42:40 +0100

    SDGpage 2

--- website/public/SDGpage.php
+++ website/public/SDGpage.php
@@ -14,6 +14,8 @@
 
   	
   </div>
+
+  
   <?php
     include("../views/footer.php");
     ?>
---
+++ website/public/SDGpage2.php
@@ -0,0 +1,44 @@
+
+<?php
+        include("../views/nav.php");
+    ?>
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>Document</title>   -->
+</head>
+<body>
+    
+
+
+  <h1>SDG 1 titel uit db</h1>
+<div class="container1">
+  <div class="img1">
+  	 <img src="https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-13.png" alt="foto"/>
+  </div>
+  <div class="text1">
+  	<h2>Subkopje uit db</h2>
+  	<p>Tekst uit db: Uitleg over lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+
+  	
+  </div>
+ 
+</body>
+</html>
+
+
+
+
+
+
+
+<?php
+    include("../views/footer.php");
+    ?>
+';}
