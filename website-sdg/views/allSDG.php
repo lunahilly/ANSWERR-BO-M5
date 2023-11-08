f6ba9962da411f7fe3398fb76eddc44a99e1dc33
SomeRandomNeko1 <114647669+SomeRandomNeko1@users.noreply.github.com>
2023-11-08 09:16:39 +0100

    fixSDGlinks

--- website-sdg/views/allSDG.php
+++ website-sdg/views/allSDG.php
@@ -3,19 +3,21 @@
 require_once("../source/connect.php");
 $connectie = db_connect();
 
-$sql = "SELECT image_path FROM sdg_cards";
+$sql = "SELECT * FROM sdg_cards"; 
 $result = mysqli_query($connectie, $sql);
 
 
 if ($result) {
 while ($row = mysqli_fetch_assoc($result)){
     $image_path = $row['image_path']; 
+    $id2 = $row['id'];
     echo '
     <figure class="groot-sdg">
-            <a href="'.$template .'?sdg='. $id .'">
+            <a href="'.$template .'?sdg='. $id2 .'">
             <img class="groot-sdg_img sdg" src= ' . $image_path .' "alt="SDG Image" ' . $sdgNummer . '>
             </a>
         </figure>';
 }
 }
+
 
